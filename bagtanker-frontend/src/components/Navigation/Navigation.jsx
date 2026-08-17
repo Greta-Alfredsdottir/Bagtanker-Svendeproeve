73d960c5cda8caf0e67bbb556e3057e4a78ca25b
Greta-Alfredsdottir <eldvakinn@hotmail.com>
2026-08-17 12:19:37 +0200

    Folders made

---
+++ bagtanker-frontend/src/components/Navigation/Navigation.jsx
@@ -0,0 +1,32 @@
+import { NavLink } from "react-router";
+import Logo from '../../assets/Logo.png'
+import style from './Navigation.module.scss';
+
+export function Navigation() {
+  return (
+    <nav className={style.navStyle}>
+      <figure>
+        <img src={Logo} alt="logo" />
+      </figure>
+
+      <ul>
+        <li>
+          <NavLink to={"/"}>Forside</NavLink>
+        </li>
+        {/* <li>
+          <NavLink to={"/"}></NavLink>
+        </li>
+        <li>
+          <NavLink to={"/"}></NavLink>
+        </li> */}
+        <li>
+          <NavLink to={"/login"}>Login</NavLink>
+        </li>
+      <li>
+        <input type="search" placeholder="søg" />
+        <button>Søg</button>
+      </li>
+      </ul>
+    </nav>
+  );
+}
